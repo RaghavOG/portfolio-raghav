5c32c0d16dd48660b569fc96205567089d2cfe51
Raghavog <04raghavsingla28@gmail.com>
2025-07-10 20:40:20 +0530

    Added Speed Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -87,6 +88,7 @@ export default function RootLayout({
         >
           {children}
             <Analytics />
+            <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
